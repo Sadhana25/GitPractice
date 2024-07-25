cd96ac9c02ff54e47c3fc5b5d8f427d6beb350aa
Sadhana25 <sadhana.bhagat5@gmail.com>
2024-07-25 14:21:25 +0530

    commit to master branch method added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // code added in maseter branch
+        add();
+    }
+    public void add(){
+        //method added
     }
 }
